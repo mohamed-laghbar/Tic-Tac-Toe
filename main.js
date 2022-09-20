147009dec28476d6c5ef051c87ad225c53af8ce0
mohamed-laghbar <simoghbaar@gmail.com>
2022-09-20 14:59:44 +0100

    basic functionality done

--- main.js
+++ main.js
@@ -2,25 +2,17 @@ function storeNames() {
   let firstName = document.getElementById("first-name").value;
   let secondName = document.getElementById("second-name").value;
 
-
-
-
   localStorage.setItem("first", firstName);
   localStorage.setItem("second", secondName);
-
- 
 }
 
 function setPlayersName()
 {
-
   let firstPlayer = document.getElementById("first-player");
   let secondPlayer = document.getElementById("second-player");
 
   firstPlayer.innerHTML =   localStorage.getItem('first');   ;
   secondPlayer.innerHTML =   localStorage.getItem('second');   ;
-
-
 }
 
 function start()
@@ -29,8 +21,36 @@ function start()
 }
 
 
+let turn = 'x';
+
+const simo = document.getElementById("selectedBox");
+
+simo.addEventListener('click', (e) => {
+
+if( e.target.tagName == 'BUTTON')
+{
+  
+  if (e.target.innerHTML == '') 
+  {
+    let currentChoise = turn ;
+    switch (e.target.innerHTML == '')
+      {
+       case currentChoise == 'x':
+          e.target.innerHTML = 'X';
+          turn = 'O';
+          console.log(' first condition' + currentChoise);
+        break;
+        case currentChoise == 'O':
+            e.target.innerHTML = 'O';
+            turn = 'x';
+            console.log(' second condition' + currentChoise);
+        break;
+      }
+  }
+}
 
 
 
+});
 
 
