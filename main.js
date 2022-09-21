21055fb2255b2889681b25e7346f2bd8f8fc4baa
mohamed-laghbar <simoghbaar@gmail.com>
2022-09-21 11:41:06 +0100

    conditions for winning are done

--- main.js
+++ main.js
@@ -63,25 +63,33 @@ if( e.target.tagName == 'BUTTON')
 let btn1 = document.getElementById('btn1').innerText;
 let btn2 = document.getElementById('btn2').innerText;
 let btn3 = document.getElementById('btn3').innerText;
-// let btn4 = document.getElementById('btn4').innerText;
-// let btn5 = document.getElementById('btn5').innerText;
-// let btn6 = document.getElementById('btn6').innerText;
-// let btn7 = document.getElementById('btn7').innerText;
-// let btn8 = document.getElementById('btn8').innerText;
-// let btn9 = document.getElementById('btn9').innerText;
+let btn4 = document.getElementById('btn4').innerText;
+let btn5 = document.getElementById('btn5').innerText;
+let btn6 = document.getElementById('btn6').innerText;
+let btn7 = document.getElementById('btn7').innerText;
+let btn8 = document.getElementById('btn8').innerText;
+let btn9 = document.getElementById('btn9').innerText;
 
- if (btn1 === btn2 && btn1 === btn3) { setTimeout(() => { alert(  'you won'); }, "700"); delete btn1,btn2,btn3; }
+if (btn1 === btn2 && btn1 === btn3 && btn1 != '' && btn2 != ''  && btn3 != ''
+ || btn4 === btn5 && btn4 === btn6 && btn4 != '' && btn5 != ''  && btn6 != ''
+ || btn7 === btn8 && btn7 === btn9 && btn7 != '' && btn8 != ''  && btn9 != ''
+ || btn1 === btn5 && btn1 === btn9 && btn1 != '' && btn5 != ''  && btn9 != ''
+ || btn3 === btn5 && btn3 === btn7 && btn3 != '' && btn5 != ''  && btn7 != ''
+ || btn1 === btn4 && btn1 === btn7 && btn1 != '' && btn4 != ''  && btn7 != ''
+ || btn2 === btn5 && btn2 === btn8 && btn2 != '' && btn5 != ''  && btn8 != ''
+ || btn3 === btn6 && btn3 === btn9 && btn3 != '' && btn6 != ''  && btn9 != '') {
+  
 
 
- 
- if (btn4 === btn5 && btn4 === btn6) { setTimeout(() => { alert(  'Won'); }, "500"); delete btn4,btn5,btn6;}
-//  if (btn7 === btn8 && btn7 === btn9) { setTimeout(() => { alert(  'Won'); }, "500"); delete btn7,btn8,btn9;}
-//  if (btn1 === btn5 && btn1 === btn9) { setTimeout(() => { alert(  'Won'); }, "500"); delete btn1,btn5,btn9;}
-//  if (btn3 === btn5 && btn3 === btn7) { setTimeout(() => { alert(  'Won'); }, "500"); delete btn3,btn5,btn7;}
-//  if (btn1 === btn4 && btn1 === btn7) { setTimeout(() => { alert(  'Won'); }, "500"); delete btn1,btn4,btn7;}
-//  if (btn2 === btn5 && btn2 === btn8) { setTimeout(() => { alert(  'Won'); }, "500"); delete btn2,btn5,btn8;}
-//  if (btn3 === btn6 && btn3 === btn9) { setTimeout(() => { alert(  'Won'); }, "500"); delete btn3,btn6,btn9;}
-
+ if (btn1 === btn2 && btn1 === btn3) { setTimeout(() => { console.log(btn1+btn2+btn3); alert( '1'); }, "700");}
+ if (btn4 === btn5 && btn4 === btn6) { setTimeout(() => { console.log(btn4+btn5+btn6); alert( '2'); }, "500");}
+ if (btn7 === btn8 && btn7 === btn9) { setTimeout(() => { console.log(btn7+btn8+btn9); alert( '3'); }, "500");}
+ if (btn1 === btn5 && btn1 === btn9) { setTimeout(() => { console.log(btn1+btn5+btn9); alert( '4'); }, "500");}
+ if (btn3 === btn5 && btn3 === btn7) { setTimeout(() => { console.log(btn3+btn5+btn7); alert( '5'); }, "500");}
+ if (btn1 === btn4 && btn1 === btn7) { setTimeout(() => { console.log(btn1+btn4+btn7); alert( '6'); }, "500");}
+ if (btn2 === btn5 && btn2 === btn8) { setTimeout(() => { console.log(btn2+btn5+btn8); alert( '7'); }, "500");}
+ if (btn3 === btn6 && btn3 === btn9) { setTimeout(() => { console.log(btn3+btn6+btn9); alert( '8'); }, "500");}
+}
 
 });
 
